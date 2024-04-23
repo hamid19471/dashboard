de9a76228c8959cb74eed3f179891681a50c83c8
Hamid Asadi <hamid.19471@gmail.com>
2024-04-23 13:55:22 +0330

    Update: identify layout

--- src/layout/identity-layout.jsx
+++ src/layout/identity-layout.jsx
@@ -1,11 +1,28 @@
 import { Outlet } from "react-router-dom";
-
+import logo from "/images/logo.png";
 const IdentityLayout = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen w-[500px] m-auto">
-      <div className="bg-slate-800 p-12 rounded-lg">
-        <Outlet />
+    <div className="flex flex-col items-center justify-between h-screen w-full m-auto">
+      <header className="h-[100px] bg-slate-800 w-full flex items-center justify-start pr-10">
+        header
+      </header>
+      <div className="flex gap-4 flex-col items-center justify-center">
+        <div className="text-center m-auto">
+          <img src={logo} alt="hamid asadi" width={180} height={43} />
+          <h2 className="font-extrabold text-slate-200 text-4xl leading-10 mt-4">
+            خوش آمدید
+          </h2>
+          <p className="font-light text-sm text-slate-400">
+            جهت استفاده از خدمات وارد شوید
+          </p>
+        </div>
+        <main className="bg-slate-800 p-12 rounded-lg w-[500px]">
+          <Outlet />
+        </main>
       </div>
+      <footer className="h-[50px] flex items-center justify-center bg-slate-800 w-full text-slate-500">
+        تمامی حقوق مادی و معنوی محفوظ است | &copy; ۱۴۰۳
+      </footer>
     </div>
   );
 };
