639596631dbcc412457f71d2ac83698b35177585
Hamid Asadi <hamid.19471@gmail.com>
2024-04-23 13:54:58 +0330

    Update: complete login page and form validation

--- src/features/indentity/login.jsx
+++ src/features/indentity/login.jsx
@@ -1,21 +1,28 @@
 import { useForm } from "react-hook-form";
 
 const Login = () => {
-  const { register, handleSubmit } = useForm({
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
     mode: "onBlur",
-    defaultValues: {},
+    defaultValues: {
+      mobile: "",
+      password: "",
+    },
   });
   const formControler = {
-    phone: {
-      require: {
+    mobile: {
+      required: {
         value: true,
         message: "لطفا شماره تماس خود را وارد کنید",
-        minLingth: 11,
-        maxLength: 11,
       },
+      minLingth: 11,
+      maxLength: 11,
     },
     password: {
-      require: {
+      required: {
         value: true,
         message: "لطفا رمز عبور خود را وارد کنید",
       },
@@ -27,22 +34,41 @@ const Login = () => {
   return (
     <div>
       <form onSubmit={handleSubmit(handleLoginSubmit)}>
-        <div>
-          <label>شماره تماس</label>
+        <div className="flex flex-col gap-2">
+          <label
+            className={`font-bold text-sm ${
+              errors.mobile && "text-red-500 text-sm"
+            }`}
+          >
+            {errors.mobile ? errors.mobile.message : "شماره تماس"}
+          </label>
           <input
-            {...register("phone", formControler)}
+            className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
+              errors.mobile && "focus:ring-red-600"
+            }`}
+            {...register("mobile", formControler.mobile)}
             type="text"
             placeholder="شماره تماس خود را وارد کنید"
           />
         </div>
-        <div>
-          <label>رمز عبور</label>
+        <div className="flex flex-col gap-2 my-4">
+          <label
+            className={`font-bold text-sm ${
+              errors.password && "text-red-500 text-sm"
+            }`}
+          >
+            {errors.password ? errors.password.message : "رمز عبور"}
+          </label>
           <input
-            {...register("password", formControler)}
+            className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
+              errors.password && "focus:ring-red-600"
+            }`}
+            {...register("password", formControler.password)}
             type="text"
             placeholder="رمز عبور خود را وارد کنید"
           />
         </div>
+        <button className="w-full bg-blue-600 py-2 rounded-lg">ورود</button>
       </form>
     </div>
   );
