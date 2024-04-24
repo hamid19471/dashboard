14e14c0e09d011223a548858b4035b8029749368
Hamid Asadi <hamid.19471@gmail.com>
2024-04-24 20:43:18 +0330

    Update: add actions for login and register and add main layout for admin panel

--- src/router.jsx
+++ src/router.jsx
@@ -2,18 +2,34 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "./features/indentity/login";
 import Register from "./features/indentity/register";
 import IdentityLayout from "./layout/identity-layout";
+import { loginActionForm } from "./actions/loginActionForm";
+import { registerActionForm } from "./actions/registerActionForm";
+import MainLayout from "./layout/main-layout";
+import Courses from "./pages/courses";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        index: true,
+        element: <Courses />,
+      },
+    ],
+  },
   {
     element: <IdentityLayout />,
     children: [
       {
         path: "login",
         element: <Login />,
+        action: loginActionForm,
       },
       {
         path: "register",
         element: <Register />,
+        action: registerActionForm,
       },
     ],
   },
