930e6675dca9e2cad5142337cae984db826566d6
Hamid Asadi <hamid.19471@gmail.com>
2024-04-24 20:43:47 +0330

    Update: login and register form and logic for register and login to application

--- src/features/indentity/login.jsx
+++ src/features/indentity/login.jsx
@@ -1,10 +1,15 @@
 import { useForm } from "react-hook-form";
+import logo from "/images/logo.png";
+import { Link, useNavigation, useSubmit } from "react-router-dom";
 
 const Login = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state !== "idle";
+  const formSubmit = useSubmit();
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm({
     mode: "onBlur",
     defaultValues: {
@@ -29,47 +34,71 @@ const Login = () => {
     },
   };
   const handleLoginSubmit = (data) => {
-    console.log(data);
+    formSubmit(data, { method: "POST" });
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit(handleLoginSubmit)}>
-        <div className="flex flex-col gap-2">
-          <label
-            className={`font-bold text-sm ${
-              errors.mobile && "text-red-500 text-sm"
+    <div className="flex flex-col items-center justify-center">
+      <div className="text-center justify-center items-center flex flex-col">
+        <img src={logo} alt="hamid asadi" width={180} height={43} />
+        <h2 className="font-extrabold text-slate-200 text-4xl leading-10 mt-4">
+          خوش آمدید
+        </h2>
+        <p className="font-light text-sm text-slate-400">
+          جهت استفاده از خدمات وارد شوید
+        </p>
+        <p className="font-bold text-white ">
+          جهت ثبت نام کلیک کنید | ‌
+          <Link className="font-bold text-blue-500" to="/register">
+            ثبت نام
+          </Link>
+        </p>
+      </div>
+      <div className="bg-slate-800 p-12 rounded-lg w-[500px] mt-8">
+        <form onSubmit={handleSubmit(handleLoginSubmit)}>
+          <div className="flex flex-col gap-2">
+            <label
+              className={`font-bold text-sm ${
+                errors.mobile && "text-red-500 text-sm"
+              }`}
+            >
+              {errors.mobile ? errors.mobile.message : "شماره تماس"}
+            </label>
+            <input
+              className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
+                errors.mobile && "focus:ring-red-600"
+              }`}
+              {...register("mobile", formControler.mobile)}
+              type="text"
+              placeholder="شماره تماس خود را وارد کنید"
+            />
+          </div>
+          <div className="flex flex-col gap-2 my-4">
+            <label
+              className={`font-bold text-sm ${
+                errors.password && "text-red-500 text-sm"
+              }`}
+            >
+              {errors.password ? errors.password.message : "رمز عبور"}
+            </label>
+            <input
+              className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
+                errors.password && "focus:ring-red-600"
+              }`}
+              {...register("password", formControler.password)}
+              type="password"
+              placeholder="رمز عبور خود را وارد کنید"
+            />
+          </div>
+          <button
+            disabled={!isValid}
+            className={`w-full bg-blue-600 py-2 rounded-lg ${
+              !isValid && "bg-blue-900 text-slate-500 pointer-events-none"
             }`}
           >
-            {errors.mobile ? errors.mobile.message : "شماره تماس"}
-          </label>
-          <input
-            className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
-              errors.mobile && "focus:ring-red-600"
-            }`}
-            {...register("mobile", formControler.mobile)}
-            type="text"
-            placeholder="شماره تماس خود را وارد کنید"
-          />
-        </div>
-        <div className="flex flex-col gap-2 my-4">
-          <label
-            className={`font-bold text-sm ${
-              errors.password && "text-red-500 text-sm"
-            }`}
-          >
-            {errors.password ? errors.password.message : "رمز عبور"}
-          </label>
-          <input
-            className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
-              errors.password && "focus:ring-red-600"
-            }`}
-            {...register("password", formControler.password)}
-            type="text"
-            placeholder="رمز عبور خود را وارد کنید"
-          />
-        </div>
-        <button className="w-full bg-blue-600 py-2 rounded-lg">ورود</button>
-      </form>
+            {isSubmitting ? "لطفا صبر کنید ..." : "ورود"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/features/indentity/register.jsx
+++ src/features/indentity/register.jsx
@@ -1,4 +1,157 @@
+import { useForm } from "react-hook-form";
+import logo from "/images/logo.png";
+import {
+  Link,
+  useActionData,
+  useNavigate,
+  useNavigation,
+  useSubmit,
+} from "react-router-dom";
+import { useEffect } from "react";
+
 const Register = () => {
-  return <div>this is Register</div>;
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state !== "idle";
+  const formSubmit = useSubmit();
+  const isSuccess = useActionData();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isSuccess) {
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000);
+    }
+  }, [isSuccess, navigate]);
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm({
+    mode: "onBlur",
+    defaultValues: {
+      mobile: "",
+      password: "",
+      confirmPassword: "",
+    },
+  });
+
+  const formControler = {
+    mobile: {
+      required: {
+        value: true,
+        message: "لطفا شماره تماس خود را وارد کنید",
+      },
+      minLength: 11,
+      maxLength: 11,
+    },
+    password: {
+      required: {
+        value: true,
+        message: "لطفا رمز عبور خود را وارد کنید",
+      },
+    },
+    confirmPassword: {
+      required: {
+        value: true,
+        message: "لطفا رمز عبور خود را وارد کنید",
+      },
+      validate: {
+        value: (value) =>
+          value === watch("password") || "رمز عبور با تکرار آن یکسان نیست",
+      },
+    },
+  };
+  const handleRegisterForm = (data) => {
+    // eslint-disable-next-line no-unused-vars
+    const { confirmPassword, ...newData } = data;
+    formSubmit(newData, { method: "POST" });
+  };
+  return (
+    <div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="text-center justify-center items-center flex flex-col">
+          <img src={logo} alt="hamid asadi" width={180} height={43} />
+          <h2 className="font-extrabold text-slate-200 text-4xl leading-10 mt-4">
+            خوش آمدید
+          </h2>
+          <p className="font-light text-sm text-slate-400">
+            جهت استفاده از خدمات ثبت نام کنید
+          </p>
+          <p className="font-bold text-white">
+            قبلا ثبت نام کرده‌اید |
+            <Link className="font-bold text-blue-500" to="/login">
+              وارد شوید
+            </Link>
+          </p>
+        </div>
+        <div className="bg-slate-800 p-12 rounded-lg w-[500px] mt-8">
+          <form onSubmit={handleSubmit(handleRegisterForm)}>
+            <div className="flex flex-col gap-2">
+              <label
+                className={`font-bold text-sm ${
+                  errors.mobile && "text-red-500 text-sm"
+                }`}
+              >
+                {errors.mobile ? errors.mobile.message : "شماره تماس"}
+              </label>
+              <input
+                className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
+                  errors.mobile && "focus:ring-red-600"
+                }`}
+                {...register("mobile", formControler.mobile)}
+                type="text"
+                placeholder="شماره تماس خود را وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col gap-2 my-4">
+              <label
+                className={`font-bold text-sm ${
+                  errors.password && "text-red-500 text-sm"
+                }`}
+              >
+                {errors.password ? errors.password.message : "رمز عبور"}
+              </label>
+              <input
+                className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
+                  errors.password && "focus:ring-red-600"
+                }`}
+                {...register("password", formControler.password)}
+                type="password"
+                placeholder="رمز عبور خود را وارد کنید"
+              />
+            </div>
+            <div className="flex flex-col gap-2 my-4">
+              <label
+                className={`font-bold text-sm ${
+                  errors.confirmPassword && "text-red-500 text-sm"
+                }`}
+              >
+                {errors.confirmPassword
+                  ? errors.confirmPassword.message
+                  : "تایید رمز عبور"}
+              </label>
+              <input
+                className={`py-3 px-2 rounded-xl mb-3 focus:ring-2 focus:ring-blue-700 focus:outline-none text-black  ${
+                  errors.confirmPassword && "focus:ring-red-600"
+                }`}
+                {...register("confirmPassword", formControler.confirmPassword)}
+                type="password"
+                placeholder="رمز عبور را مجدد وارد کنید"
+              />
+            </div>
+            <button
+              disabled={!isValid}
+              className={`w-full bg-blue-600 py-2 rounded-lg ${
+                !isValid && "bg-blue-800 text-slate-500 pointer-events-none"
+              }`}
+            >
+              {isSubmitting ? "در حال ثبت نام ..." : "ثبت نام"}
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Register;
