633dec66812ab569a17b7348b234b566dba4bf14
Hamid Asadi <hamid.19471@gmail.com>
2024-04-26 20:43:21 +0330

    Update: add change language component in header section of identity layout

--- src/layout/identity-layout.jsx
+++ src/layout/identity-layout.jsx
@@ -1,9 +1,12 @@
+import { useTranslation } from "react-i18next";
 import { Outlet } from "react-router-dom";
+import ChangeLanguage from "../components/change-language";
 const IdentityLayout = () => {
+  const { t } = useTranslation();
   return (
     <div className="flex flex-col items-center justify-between h-screen w-full m-auto">
-      <header className="h-[100px] bg-slate-800 w-full flex items-center justify-start pr-10">
-        header
+      <header className="h-[100px] bg-slate-800 w-full flex items-center justify-start px-10">
+        <ChangeLanguage />
       </header>
       <div className="flex gap-4 flex-col items-center justify-center">
         <main className="">
@@ -11,7 +14,7 @@ const IdentityLayout = () => {
         </main>
       </div>
       <footer className="h-[50px] flex items-center justify-center bg-slate-800 w-full text-slate-500">
-        تمامی حقوق مادی و معنوی محفوظ است | &copy; ۱۴۰۳
+        {t("footer.title")}
       </footer>
     </div>
   );
