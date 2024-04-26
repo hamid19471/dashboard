611c8a5d50d6734eea1ff8ec6e6a6e3a80fd5666
Hamid Asadi <hamid.19471@gmail.com>
2024-04-26 23:25:17 +0330

    Update: add route gurd

--- src/layout/identity-layout.jsx
+++ src/layout/identity-layout.jsx
@@ -1,8 +1,16 @@
 import { useTranslation } from "react-i18next";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import ChangeLanguage from "../components/change-language";
+import { useEffect } from "react";
 const IdentityLayout = () => {
   const { t } = useTranslation();
+  const token = localStorage.getItem("token");
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (token) {
+      navigate("/");
+    }
+  }, [token]);
   return (
     <div className="flex flex-col items-center justify-between h-screen w-full m-auto">
       <header className="h-[100px] bg-slate-800 w-full flex items-center justify-start px-10">
