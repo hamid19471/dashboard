a8c75737e1840035676f3b201fd29ab7027ca3e1
Hamid Asadi <hamid.19471@gmail.com>
2024-04-26 20:42:17 +0330

    Create: main app context and app reducer and config both in this step for multi language global state

---
+++ src/context/app-context.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useEffect, useReducer } from "react";
+import appReducer from "./app-reducer";
+import { useTranslation } from "react-i18next";
+
+const AppContext = createContext();
+const initialState = {
+  language: localStorage.getItem("language") || "fa",
+};
+const AppProvider = ({ children }) => {
+  const { i18n } = useTranslation();
+  const [state, dispatch] = useReducer(appReducer, initialState);
+  const changeLanguage = (language) => {
+    dispatch({ type: "CHANGE_LANGUAGE", payload: language });
+  };
+  useEffect(() => {
+    i18n.changeLanguage(state.language);
+    localStorage.setItem("language", state.language);
+    document.dir = state.language === "fa" ? "rtl" : "ltr";
+  }, [state.language]);
+  return (
+    <AppContext.Provider value={{ ...state, changeLanguage }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+
+export { useAppContext, AppProvider };
