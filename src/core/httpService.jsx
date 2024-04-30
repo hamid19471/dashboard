92b36b6d272f631533b8bd67320df7b0d3248c8d
Hamid Asadi <hamid.19471@gmail.com>
2024-04-30 11:16:43 +0330

    Update: add interceptor request and response for handle authententication fetch

--- src/core/httpService.jsx
+++ src/core/httpService.jsx
@@ -5,3 +5,31 @@ const BASE_URL = "https://react-mini-projects-api.classbon.com";
 export const httpService = axios.create({
   baseURL: BASE_URL,
 });
+
+export const authHttpService = axios.create({
+  baseURL: BASE_URL,
+});
+
+authHttpService.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers = {
+        authorization: `Bearer ${token}`,
+      };
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+authHttpService.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error?.response?.status === 401) {
+      localStorage.removeItem("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
