7136f5eb70f03b682b35d0381c28ffc9d6ddae90
Hamid Asadi <hamid.19471@gmail.com>
2024-04-30 11:18:23 +0330

    Update: fix some styles issues

--- src/layout/main-layout/footer.jsx
+++ src/layout/main-layout/footer.jsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 const Footer = () => {
   const { t } = useTranslation();
   return (
-    <footer className="h-[50px] flex items-center justify-center bg-slate-800 w-full text-slate-500">
+    <footer className="h-[50px] flex items-center justify-center bg-slate-800 w-full text-slate-500 ">
       {t("footer.title")}
     </footer>
   );
