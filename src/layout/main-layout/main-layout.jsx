eee5bcd2b31325a90afe450aec0bd94249624e0e
Hamid Asadi <hamid.19471@gmail.com>
2024-04-30 11:17:44 +0330

    Update: fix some style issues and remove useEffect used before

--- src/layout/main-layout/main-layout.jsx
+++ src/layout/main-layout/main-layout.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import Sidebar from "./sidebar";
 import Header from "./header";
@@ -7,17 +6,15 @@ import Footer from "./footer";
 const MainLayout = () => {
   const token = localStorage.getItem("token");
   const navigate = useNavigate();
-  useEffect(() => {
-    if (!token) {
-      navigate("/login");
-    }
-  }, [token]);
+  if (!token) {
+    navigate("/login");
+  }
   return (
     <div className="flex w-full h-screen relative">
       <Sidebar />
-      <div className="w-full z-10 flex flex-col">
+      <div className="w-full z-10 flex flex-col justify-between">
         <Header />
-        <main className="flex gap-4 flex-col h-full my-8 mx-4">
+        <main className="flex gap-4 flex-col h-screen overflow-scroll my-8 mx-4">
           <Outlet />
         </main>
         <Footer />
