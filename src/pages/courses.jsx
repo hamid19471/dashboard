a01ffc4a7685753787028acb0545ce507ceaa48d
Hamid Asadi <hamid.19471@gmail.com>
2024-04-30 11:17:04 +0330

    Update: couse rout and page

--- src/pages/courses.jsx
+++ src/pages/courses.jsx
@@ -1,5 +1,29 @@
+import { Await, useLoaderData } from "react-router-dom";
+import CourseList from "../features/courses/component/course-list";
+import { Suspense } from "react";
+
 const Courses = () => {
-  return <h1>Courses</h1>;
+  const data = useLoaderData();
+  return (
+    <div>
+      <button className="px-6 py-2 bg-blue-600 text-white font-semibold text-sm rounded-lg hover:bg-blue-700">
+        افزودن دوره جدید
+      </button>
+      <div className="flex items-center justify-start gap-2">
+        <Suspense
+          fallback={
+            <p className="text-blue-500 font-bold text-sm mt-12">
+              درحال دریافت اطلاعات...
+            </p>
+          }
+        >
+          <Await resolve={data.courses}>
+            {(courses) => <CourseList courses={courses} />}
+          </Await>
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default Courses;
