4775217764615ca17623d125e44218ae22e4c634
Hamid Asadi <hamid.19471@gmail.com>
2024-05-03 00:36:38 +0330

    Create: course loader for fetch data using react router dom

---
+++ src/features/data-loader/course-loader.jsx
@@ -0,0 +1,10 @@
+import { defer } from "react-router-dom";
+import { authHttpService } from "../../core/httpService";
+export const CourseLoader = async () => {
+  return defer({ courses: loadCourses() });
+};
+
+const loadCourses = async () => {
+  const response = await authHttpService.get("/Course/list");
+  return response.data;
+};
