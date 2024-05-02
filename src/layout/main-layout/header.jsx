8d2e244910fe5cd82247d128bdbcca6c1b1f930c
Hamid Asadi <hamid.19471@gmail.com>
2024-05-03 00:37:01 +0330

    Fix: issue about header icon

--- src/layout/main-layout/header.jsx
+++ src/layout/main-layout/header.jsx
@@ -1,13 +1,13 @@
 import { useAppContext } from "../../context/app-context";
-import { HiBars3BottomRight } from "react-icons/hi2";
+import { LiaBarsSolid } from "react-icons/lia";
 import ChangeLanguage from "../../components/change-language";
 const Header = () => {
   const { toggleSidebar } = useAppContext();
   return (
     <header className="h-[100px] gap-4 bg-slate-800 w-full flex items-center justify-start px-2">
-      <button onClick={toggleSidebar}>
-        <HiBars3BottomRight className="w-8 h-8 cursor-pointer" />
-      </button>
+      <a onClick={toggleSidebar}>
+        <LiaBarsSolid className="w-8 h-8 cursor-pointer" />
+      </a>
       <ChangeLanguage />
     </header>
   );
